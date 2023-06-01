ff11421e3332bc9b80097f167df7750c81d1e978
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-01 14:32:42 +0800

    Update error.R

--- R/error.R
+++ R/error.R
@@ -1,17 +1,16 @@
-
 # This file contains exception checking and error handling functions
 
 #
 # exception handling function for malformed input in lindia
 #
-handle_exception <- function(input, function_name){
-   
-   type = class(input)
-   
-   # exception handling : input not lm object
-   if (type != "lm"){
-      stop(paste(function_name, "doesn't know how to handle non-lm object"))
-   }
-   
+handle_exception <- function(input, function_name) {
+  
+  type = class(input)
+  
+  # exception handling: input not lm object
+  if (type != "lm") {
+    function_name |> 
+      paste("doesn't know how to handle non-lm object") |> 
+      stop()
+  }
 }
-
