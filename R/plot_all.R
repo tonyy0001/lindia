0c9380324d6b3dd71f5cdaedf7f827e64276a1e3
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-03 19:57:15 +0800

    Update plot_all.R

--- R/plot_all.R
+++ R/plot_all.R
@@ -2,20 +2,18 @@
 #'
 #' @param plots a list of plots
 #' @param ncol numeric; the number of column that the arranged grid need to be.
-#'              defaults to fitting all plots in square matrix
+#' defaults to fitting all plots in square matrix
 #' @param max.per.page numeric; maximum number of plots allowed in one page.
-#' 
 #' @return plots in a given list arrangeed using gridExtra
 #' @examples 
-#'          library(MASS)
-#'          data(Cars93)
-#'          # a regression with categorical variable
-#'          cars_lm <- lm(Price ~ Passengers + Length + RPM + Origin, data = Cars93)
-#'          plots <- gg_diagnose(cars_lm, plot.all = FALSE)
-#'          names(plots)
-#'          selected.plots <- plots[-c(2, 5)]
-#'          plot_all(selected.plots)
-#'          
+#' library(MASS)
+#' data(Cars93)
+#' # a regression with categorical variable
+#' cars_lm <- lm(Price ~ Passengers + Length + RPM + Origin, Cars93)
+#' p <- gg_diagnose(cars_lm, plot.all = FALSE)
+#' names(p)
+#' selected.plots <- plots[-c(2, 5)]
+#' plot_all(selected.plots)
 #' @export
 plot_all <- function(plots, ncol = NA, max.per.page = NA) {
   
