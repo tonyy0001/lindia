811d537a9433a0f5d1305384c78f6a89076959a1
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-03 19:55:00 +0800

    Update gg_scalelocation.R

--- R/gg_scalelocation.R
+++ R/gg_scalelocation.R
@@ -1,18 +1,17 @@
 #' Plot scale-location (also called spread-location plot) in ggplot.
 #'
 #' @param fitted.lm a fitted linear model (i.e. lm, glm) that contains fitted regression
-#' @param method smoothing method of fitted line on scale-location plot.
-#'          eg. "lm", "glm", "gam", "loess", "rlm". See \url{http://docs.ggplot2.org/current/geom_smooth.html}
-#'          for more details.
+#' @param method smoothing method of fitted line on scale-location plot.eg. "lm", "glm", "gam", "loess", "rlm". 
+#' See \url{http://docs.ggplot2.org/current/geom_smooth.html} for more details.
 #' @param se logical; determines whether se belt should be plotted on plot
 #' @param scale.factor numeric; scales the point size and linewidth to allow customized viewing. Defaults to 1.
 #' @return A ggplot object that contains scale-location graph
-#' @examples library(MASS)
+#' @examples 
+#' library(MASS)
 #' data(Cars93)
-#' cars_lm <- lm(Price ~ Passengers + Length + RPM, data = Cars93)
+#' cars_lm <- lm(Price ~ Passengers + Length + RPM, Cars93)
 #' gg_scalelocation(cars_lm)
 #' @export
-#'
 gg_scalelocation <- function(fitted.lm, method = 'loess', scale.factor = 1, se = FALSE) {
   
   handle_exception(fitted.lm, "gg_scalelocation")
