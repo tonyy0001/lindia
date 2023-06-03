07cc21669b1fe467204ef5f160df0b12534e2c40
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-03 19:44:53 +0800

    Update gg_diagnose.R

--- R/gg_diagnose.R
+++ R/gg_diagnose.R
@@ -22,9 +22,13 @@
 #' gg_diagnose(cars_lm)
 #' # customize which diagnostic plot is included
 #' plots <- gg_diagnose(cars_lm, plot.all = FALSE)
-#' names(plots)     # get name of the plots
-#' exclude_plots <- plots[-c(1, 3) ]    #exclude certain diagnostics plots
-#' include_plots <- plots[c(1, 3)]      # include certain diagnostics plots
+#' get name of the plots
+#' names(plots)
+#' # exclude certain diagnostics plots
+#' exclude_plots <- plots[-c(1, 3)]
+#' # include certain diagnostics plots
+#' include_plots <- plots[c(1, 3)]
+# make use of plot_all() in lindia
 #' plot_all(exclude_plots)              # make use of plot_all() in lindia
 #' plot_all(include_plots)
 #' @export
